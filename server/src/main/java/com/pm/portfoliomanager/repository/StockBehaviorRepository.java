ad341e8ca54f8f770fff455c53a3588095fce219
Ayush Katre <ayushkatre1801@gmail.com>
2026-01-31 10:42:30 +0530

    sending data to serverless postgreSQL using JPA

---
+++ server/src/main/java/com/pm/portfoliomanager/repository/StockBehaviorRepository.java
@@ -0,0 +1,8 @@
+package com.pm.portfoliomanager.repository;
+
+import com.pm.portfoliomanager.model.StockBehavior;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface StockBehaviorRepository extends JpaRepository<StockBehavior, String> {}
