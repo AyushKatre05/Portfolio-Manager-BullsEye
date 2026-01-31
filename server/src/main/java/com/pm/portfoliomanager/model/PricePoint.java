41b9a09d699906001b24602f22c18384cf8523fe
Ayush Katre <ayushkatre1801@gmail.com>
2026-01-31 10:48:11 +0530

    pricepoint entity declarations over serverless postgres

--- server/src/main/java/com/pm/portfoliomanager/model/PricePoint.java
+++ server/src/main/java/com/pm/portfoliomanager/model/PricePoint.java
@@ -1,5 +1,9 @@
 package com.pm.portfoliomanager.model;
 
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
 import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
@@ -7,7 +11,18 @@ import lombok.NoArgsConstructor;
 @Data
 @AllArgsConstructor
 @NoArgsConstructor
+@Entity
 public class PricePoint {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
     private String date;
     private double close;
+
+    // Custom constructor without id
+    public PricePoint(String date, double close) {
+        this.date = date;
+        this.close = close;
+    }
 }
